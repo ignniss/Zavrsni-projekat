be2828f5d2d8599340cbfe468b44bb945f5a308d
brankosavkovic <brainacc@gmail.com>
2019-09-04 23:06:22 +0200

    Odradio svaki program posebno

--- Script/training.js
+++ Script/training.js
@@ -1,32 +1,47 @@
 $(document).ready(() => {
 
+    let saznajCrossConditioning = $("#saznajViseCrossConditioning");
+    let saznajCrossTraining = $("#saznajViseCrossTraining");
+    let saznajCrossBox = $("#saznajViseCrossBox");
     let saznajZumba = $("#saznajViseZumba");
     let saznajPilates = $("#saznajVisePilates");
-
     let saznajYoga = $("#saznajViseYoga");
 
     let naslov = $("#naslov");
+    let crossConditioningDiv = $("#kontejnerCrossConditioning");
+    let crossTrainingDiv = $("#kontejnerCrossTraining");
+    let crossBoxDiv = $("#kontejnerCrossBox");
     let mainDiv = $("#kontejner");
     let zumbaDiv = $("#kontejnerZumba");
     let pilatesDiv = $("#kontejnerPilates");
     let yogaDiv = $("#kontejnerYoga");
 
+    let programiCrossConditioning = $("#programiCrossConditioning");
+    let programiCrossTraining = $("#programiCrossTraining");
+    let programiCrossBox = $("#programiCrossBox");
     let programiZumba = $("#programiZumba");
     let programiPilates = $("#programiPilates");
     let programiYoga = $("#programiYoga");
 
-
-    saznajPilates.click(() => {
+    saznajCrossTraining.click(() => {
         mainDiv.css("display","none");
-        pilatesDiv.css("display","flex");
-        naslov.text("Pilates");
+        crossTrainingDiv.css("display","flex");
+        naslov.text("Cross training");
         naslov.css({"font-size":"25px", "font-weight":"bold", "text - transform": "uppercase"});
     });
 
+    saznajCrossConditioning.click(() => {
+        mainDiv.css("display","none");
+        crossConditioningDiv.css("display","flex");
+        naslov.text("Cross conditioning");
+        naslov.css({"font-size":"25px", "font-weight":"bold", "text - transform": "uppercase"});
+    });
 
-    programiPilates.click(() => {
-        mainDiv.css("display", "flex");
-        pilatesDiv.css("display", "none");
+    saznajCrossBox.click(() => {
+        mainDiv.css("display","none");
+        crossBoxDiv.css("display","flex");
+        naslov.text("Cross box");
+        naslov.css({"font-size":"25px", "font-weight":"bold", "text - transform": "uppercase"});
     });
 
     saznajZumba.click(() => {
@@ -36,9 +51,11 @@ $(document).ready(() => {
         naslov.css({"font-size":"25px", "font-weight":"bold",  "text - transform":"uppercase"});
     });
 
-    programiZumba.click(() => {
-        mainDiv.css("display","flex");
-        zumbaDiv.css("display","none");
+    saznajPilates.click(() => {
+        mainDiv.css("display","none");
+        pilatesDiv.css("display","flex");
+        naslov.text("Pilates");
+        naslov.css({"font-size":"25px", "font-weight":"bold", "text - transform": "uppercase"});
     });
 
     saznajYoga.click(() => {
@@ -48,10 +65,28 @@ $(document).ready(() => {
         naslov.css({"font-size":"25px", "font-weight":"bold", "text - transform": "uppercase"});
     });
 
-    programiYoga.click(() => {
-        mainDiv.css("display","flex");
-        yogaDiv.css("display","none");
-    });
+
+    // Zasto radi i bez ovoga?!
+
+    // programiCrossBox.click(() => {
+    //     mainDiv.css("display","flex");
+    //     crossBoxDiv.css("display","none");
+    // });
+
+    // programiZumba.click(() => {
+    //     mainDiv.css("display","flex");
+    //     zumbaDiv.css("display","none");
+    // });
+
+    // programiPilates.click(() => {
+    //     mainDiv.css("display", "flex");
+    //     pilatesDiv.css("display", "none");
+    // });
+
+    // programiYoga.click(() => {
+    //     mainDiv.css("display","flex");
+    //     yogaDiv.css("display","none");
+    // });
 
 })
 
