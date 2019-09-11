370a5cea42736931c7076aade05007e742d32ba3
brankosavkovic <brainacc@gmail.com>
2019-09-11 10:19:29 +0200

    Promenio izgled stranice, potrebno je još malo dorade za toggle

--- Script/about.js
+++ Script/about.js
@@ -1,10 +1,14 @@
 $(document).ready(() => {
-    $('.btn').click(() => {
+    let x = 0;
+    $("#flip").click(() => {
+        x++;
+        $("#panel").slideToggle();
+        if(x % 2 == 0){
+            $('#flip').text('saznaj vise...');
+        }
+        else{
+            $('#flip').text('saznaj manje...');
+        }
+    });
+});
 
-        let tekst = $('.text');
-        let napisi = tekst[0].children('p');
-        // $('#klik').text(tekst[2]);
-        console.log(napisi);
-    })
-
-})
