2ae4620e792f174c2d2a19c99b787a601fe2febf
AlexStamenkovic <49113682+AlexStamenkovic@users.noreply.github.com>
2019-10-19 23:46:07 +0200

    Add files via upload

---
+++ neuspela_konekcija.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Style/bootstrap.css">
+    <link rel="stylesheet" href="Style/style.css">
+    <link rel="stylesheet" href="Style/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+    <title>Naslovna strana</title>
+</head>
+
+<body>
+    <nav id="navBar" class="navbar text-uppercase navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container">
+
+            <a class="navbar-brand text-success font-weight-bolder" href="index.php"><img id="logo" src="Img/logo.png" alt=""> ozon cross gym </a>
+
+
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link text-success" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="training.php">cross gym training</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cross_sport_kids.php">cross sport kids</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">about us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gallery.php">gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">contact us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">login/Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <img id="ser" src="Img/ser.png" alt="SER">
+                        <img id="uk" src="Img/uk.png" alt="ENG">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<?php
+    
+    echo "<p style='color:black;font-weight:bold;text-align:center'>Ups! Nesto se desilo...
+    Molimo pokusajte ponovo da se registrujete!</p>";
+
+
+?>
+
+<div id="footer" class="bg-dark">
+        <h2>REKLAME</h2>
+    </div>
+</body>
+
+</html>
