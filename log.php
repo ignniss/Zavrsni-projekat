de9ebb77ef7b77100a8415971db660bee9ad7800
AlexStamenkovic <49113682+AlexStamenkovic@users.noreply.github.com>
2019-10-22 00:03:54 +0200

    Update log.php

--- log.php
+++ log.php
@@ -1,24 +1,42 @@
-1<?php
+<?php
 session_start();
+
+function generisi_pomocnu_lozinku() {
+    $loz=array();
+    $niska='0123456789mnbvcxzlkjhgfdsapoiuytrewqMNBVCXZLKJHGFDSAPOIUYTREWQ';
+    for($i=0;$i<5;$i++){
+        $char=$niska[rand(0,61)];
+        array_push($loz,$char);
+    }
+    return implode('',$loz);
+}
+
+
 if(isset($_POST['btnLog'])){
     require "connection.php";
-
     $korisnickoIme = $_POST['korisnicko'];
     $password = $_POST['pass'];
 
     $sql = "SELECT id FROM users WHERE korisnicko_ime = '$korisnickoIme' AND sifra = '$password'";
     $query = mysqli_query($conn, $sql);
-
-    $id = mysqli_fetch_assoc($query)['id'];;
-
-    if($id){
-        $_SESSION['id'] = $id;
-        
-        header("location: profile.php");
-    }else{
-        header("location: login.php");
-        // echo "ne valja";
-    } 
+    $id = mysqli_fetch_assoc($query)['id'];
+        if($id){
+            $_SESSION['id'] = $id;       
+            header("location: profile.php");
+            } else{
+        $sql2 = "SELECT Email FROM users WHERE korisnicko_ime = '$korisnickoIme'";
+        $query = mysqli_query($conn, $sql2);
+        $email = mysqli_fetch_assoc($query)['Email'];
+        $nova_sifra=generisi_pomocnu_lozinku();
+        $filename='nova_lozinka.txt';
+        $ispis=$nova_sifra."\n".$email."\n".$korisnickoIme;
+        file_put_contents($filename,$ispis);
+        $sql3="UPDATE users SET sifra='$nova_sifra' WHERE korisnicko_ime='$korisnickoIme'";
+        $query=mysqli_query($conn, $sql3);
+        header('Location:oporavak_lozinke.php');
+        }
+       
 }
 
 ?>
+
