24dbdb8656ab8f4a97e65f6bcbe02ce86d5b5537
AlexStamenkovic <49113682+AlexStamenkovic@users.noreply.github.com>
2019-10-23 20:03:51 +0200

    Update oporavak_lozinke.php

--- oporavak_lozinke.php
+++ oporavak_lozinke.php
@@ -111,7 +111,7 @@ $mail->SMTPSecure = "tls";
 $mail->Port = 587;                                   
 
 $mail->From = "aleksandrastamenkovic1004@gmail.com";
-$mail->FromName = "AleksandraStamenkovic";
+$mail->FromName = "Ozon Cross Gym";";
 
 $mail->addAddress($email, $korisnik);
 
