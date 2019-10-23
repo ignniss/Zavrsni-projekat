be04c365d5b462d07aa3385909619336ba8e44dd
ignniss <49113643+ignniss@users.noreply.github.com>
2019-10-23 18:21:19 +0200

    potvrda brisanja

--- Script/search.js
+++ Script/search.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $('.brisanje').click(() => {
         if (!confirm("DA LI ZAISTA ŽELITE DA IZBRIŠETE KORISNIKA?")) {
-            $('#delete_link').attr('href', '#');
+            $('.delete_link').removeAttr("href");
             location.reload();
         }
 
